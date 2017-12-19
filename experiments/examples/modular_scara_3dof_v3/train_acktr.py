86488e0141fa8886771551ee9036990e2b73ed29
rkojcev <rkojcev@gmail.com>
2017-12-19 14:14:08 +0100

    adding monitoring to the train ppo and train acktr.

--- experiments/examples/modular_scara_3dof_v3/train_acktr.py
+++ experiments/examples/modular_scara_3dof_v3/train_acktr.py
@@ -11,7 +11,15 @@ from baselines.acktr.policies import GaussianMlpPolicy
 from baselines.acktr.value_functions import NeuralNetValueFunction
 from baselines.common import set_global_seeds
 
+from baselines import bench, logger
+import os
+
 env = gym.make('GazeboModularScara3DOF-v3')
+logdir = '/tmp/rosrl/' + str(env.__class__.__name__) +'/acktr/monitor/'
+logger.configure(os.path.abspath(logdir))
+print("logger.get_dir(): ", logger.get_dir() and os.path.join(logger.get_dir()))
+env = bench.MonitorRobotics(env, logger.get_dir() and os.path.join(logger.get_dir()), allow_early_resets=True) #, allow_early_resets=True
+
 initial_observation = env.reset()
 print("Initial observation: ", initial_observation)
 env.render()
