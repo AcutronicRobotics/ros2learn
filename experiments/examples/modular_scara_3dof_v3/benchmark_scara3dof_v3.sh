6913a6f7357cf7a9e891fdf9d7def552b856871e
rkojcev <rkojcev@gmail.com>
2018-01-15 13:37:41 +0100

    adding scripts to launch with different times.

--- experiments/examples/modular_scara_3dof_v3/benchmark_scara3dof_v3.sh
+++ experiments/examples/modular_scara_3dof_v3/benchmark_scara3dof_v3.sh
@@ -3,11 +3,8 @@
 TIMESTEPS=1e6
 ENVIRONMENT="GazeboModularScara3DOF-v3"
 SLOWNESS=1
+
 SLOWNESS_UNIT='sec'
-# SLOWNESS_UNIT= 'nsec'
-# PPO1 benchmark
-# python3 train_ppo1.py  --slowness $SLOWNESS --slowness_unit $SLOWNESS_UNIT
-# PPO2 benchmark
 python3 train_ppo2.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
 
 SLOWNESS_UNIT='nsec'
@@ -22,12 +19,3 @@ python3 train_ppo2.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
 #train for 1ms
 SLOWNESS=1000000
 python3 train_ppo2.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-# ACKTR benchmark
-# python3 train_acktr.py --slowness $SLOWNESS --slowness_unit $SLOWNESS_UNIT
-# VPG benchmark
-# python3 train_vpg.py --environment $ENVIRONMENT --num_timesteps $TIMESTEPS
-# TRPO benchmark
-# python3 train_trpo.py #--environment $ENVIRONMENT --num_timesteps $TIMESTEPS
-# DDPG benchmark
-# python3 train_ddpg.py #--environment $ENVIRONMENT --num_timesteps $TIMESTEPS
