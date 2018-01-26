ef22c83a238666d4505b71ed9ebe91dec6978d9e
rkojcev <rkojcev@gmail.com>
2018-01-26 16:37:38 +0100

    increase training time

--- experiments/examples/modular_scara_3dof_v3/train_mlsh.py
+++ experiments/examples/modular_scara_3dof_v3/train_mlsh.py
@@ -120,8 +120,8 @@ if __name__ == '__main__':
     macro_duration = 10
     num_subs = 6
     num_rollouts = 2500
-    warmup_time = 30 #1 # 30
-    train_time = 200 #2 # 200
+    warmup_time = 50 #1 # 30
+    train_time = 250 #2 # 200
     force_subpolicy=None
     store=True
 
