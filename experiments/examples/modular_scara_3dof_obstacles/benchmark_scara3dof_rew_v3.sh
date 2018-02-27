adadf5e6c1b7b1d248ff68cc528c3c2e39a48b2d
rkojcev <rkojcev@gmail.com>
2018-02-27 10:01:56 +0100

    adding some examples from roboshool for scara (need to add roboschool to our env.)

--- experiments/examples/modular_scara_3dof_obstacles/benchmark_scara3dof_rew_v3.sh
+++ experiments/examples/modular_scara_3dof_obstacles/benchmark_scara3dof_rew_v3.sh
@@ -1,124 +1,107 @@
-#!/bin/sh
-SLOWNESS_UNIT='nsec'
-TARGET=1
-PENALIZED_REWARD=1
-#train for 100ms
-SLOWNESS=1000000
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='nsec'
-TARGET=1
-PENALIZED_REWARD=2
-#train for 100ms
-SLOWNESS=10000000
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+# # !/bin/sh
+# SLOWNESS_UNIT='nsec'
+# #train for 1ms
+# SLOWNESS=1000000
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='nsec'
+# #train for 10ms
+# SLOWNESS=10000000
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
 
 SLOWNESS_UNIT='nsec'
-TARGET=1
-PENALIZED_REWARD=3
 #train for 100ms
 SLOWNESS=100000000
 python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
 
 killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
 
-SLOWNESS_UNIT='sec'
-TARGET=1
-PENALIZED_REWARD=4
+# SLOWNESS_UNIT='sec'
+# #train for 1s
+# SLOWNESS=1
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+
+SLOWNESS_UNIT='nsec'
 #train for 100ms
-SLOWNESS=1
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+SLOWNESS=100000000
+python3 train_ppo2.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
 
 
 killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
 
-SLOWNESS_UNIT='sec'
-TARGET=1
-PENALIZED_REWARD=5
-#train for 100ms
-SLOWNESS=2
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+# SLOWNESS_UNIT='sec'
+# #train for 2s
+# SLOWNESS=2
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# SLOWNESS_UNIT='sec'
+# #train for 3s
+# SLOWNESS=3
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='sec'
+# #train for 4s
+# SLOWNESS=4
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='sec'
+# #train for 5s
+# SLOWNESS=5
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
 
-
-SLOWNESS_UNIT='sec'
-TARGET=2
-PENALIZED_REWARD=1
-#train for 100ms
-SLOWNESS=3
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=2
-PENALIZED_REWARD=2
-#train for 100ms
-SLOWNESS=4
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=2
-PENALIZED_REWARD=3
-#train for 100ms
-SLOWNESS=5
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=2
-PENALIZED_REWARD=4
-#train for 100ms
-SLOWNESS=6
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=2
-PENALIZED_REWARD=5
-#train for 100ms
-SLOWNESS=7
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=2
-PENALIZED_REWARD=3
-#train for 100ms
-SLOWNESS=8
-python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=1
-PENALIZED_REWARD=3
-#train for 100ms
-SLOWNESS=9
-python3 train_ppo1.py --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
-
-killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
-
-SLOWNESS_UNIT='sec'
-TARGET=1
-PENALIZED_REWARD=3
-#train for 100ms
-SLOWNESS=10
-python3 train_ppo1.py --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+# SLOWNESS_UNIT='sec'
+# #train for 6s
+# SLOWNESS=6
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='sec'
+# #train for 7s
+# SLOWNESS=7
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='sec'
+#
+# #train for 8s
+# SLOWNESS=8
+# python3 train_ppo1.py  --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+#
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='sec'
+# #train for 9s
+# SLOWNESS=9
+# python3 train_ppo1.py --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
+#
+# killall -9 roscore rosmaster roslaunch robot_state_publisher scara_contact_plugin gzclient gzserver
+#
+# SLOWNESS_UNIT='sec'
+# #train for 10s
+# SLOWNESS=10
+# python3 train_ppo1.py --slowness $SLOWNESS  --slowness-unit $SLOWNESS_UNIT
