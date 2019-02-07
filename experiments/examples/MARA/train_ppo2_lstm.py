7b358f5b72842af0d83ceb6c6182a092f0b46d99
yueerro <yue.trbj@gmail.com>
2019-02-07 16:30:32 +0100

    remove duplicated logger

--- experiments/examples/MARA/train_ppo2_lstm.py
+++ experiments/examples/MARA/train_ppo2_lstm.py
@@ -66,7 +66,6 @@ alg_kwargs = get_learn_function_defaults('ppo2', env_type)
 
 # Create needed folders
 logdir = '/tmp/ros_rl2/' + alg_kwargs['env_name'] + '/ppo2_lstm/'
-logger.configure( os.path.abspath(logdir) )
 
 # Generate tensorboard file
 format_strs = os.getenv('MARA_LOG_FORMAT', 'stdout,log,csv,tensorboard').split(',')
